77563f1e1c812ed0069912f8bf9307a8c1d980e2
Aimee LaPlant <aimee@aimeelaplant.com>
2018-10-25 21:53:25 -0400

    Get appearances for characters list.

--- web/controllers.go
+++ web/controllers.go
@@ -62,15 +62,14 @@ func (c CharacterController) Character(ctx echo.Context) error {
 	if character == nil {
 		return JSONNotFound(ctx)
 	}
-	apps, err := c.characterSvc.ListAppearances(slug)
+	cStrct, err := c.withAppearances(character.Slug)
 	if err != nil {
 		return JSONServerError(ctx)
 	}
-	characterModel := NewCharacter(*character, apps)
-	return JSONDetailViewOK(ctx, characterModel)
+	return JSONDetailViewOK(ctx, cStrct)
 }
 
-// Characters lists characters and can filter by publisher with `?publisher=marvel`.
+// Characters lists the characters.
 func (c CharacterController) Characters(ctx echo.Context) error {
 	var results []*comic.Character
 	page, err := pageNumber(ctx)
@@ -78,19 +77,32 @@ func (c CharacterController) Characters(ctx echo.Context) error {
 		return err
 	}
 	var slugs []comic.PublisherSlug
-	publisher := comic.PublisherSlug(ctx.QueryParam("publisher"))
-	if publisher != "" {
-		slugs = []comic.PublisherSlug{publisher}
-	}
 	results, err = c.characterSvc.CharactersByPublisher(slugs, true, 25+1, (page-1)*25)
 	if err != nil {
 		return JSONServerError(ctx)
 	}
 	var data = make([]interface{}, len(results))
 	for i, v := range results {
-		data[i] = v
+		character, err := c.withAppearances(v.Slug)
+		if err != nil {
+			return JSONServerError(ctx)
+		}
+		data[i] = character
 	}
 	return JSONListViewOK(ctx, data, 25)
+}
+
+// Gets a character struct with the appearances attached.
+func (c CharacterController) withAppearances(slug comic.CharacterSlug) (Character, error) {
+	character, err := c.characterSvc.Character(slug)
+	if err != nil || character == nil {
+		return Character{}, err
+	}
+	apps, err := c.characterSvc.ListAppearances(slug)
+	if err != nil {
+		return Character{}, err
+	}
+	return NewCharacter(*character, apps), nil
 }
 
 // Gets the page number from the query parameter `page` with default value if empty.
@@ -100,9 +112,6 @@ func pageNumber(ctx echo.Context) (int, error) {
 		page, err := strconv.Atoi(query)
 		if err != nil {
 			return 1, JSONBadRequest(ctx, "malformed `page` parameter")
-		}
-		if page == 1 {
-			return 1, nil
 		}
 		return page, nil
 	}
