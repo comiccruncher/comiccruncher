c6bacfb980dc6e7d9c8fd793660d17ca0c18ec3f
Aimee LaPlant <aimee@aimeelaplant.com>
2018-12-19 00:05:00 -0500

    added slug :u

--- web/models_test.go
+++ web/models_test.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"encoding/json"
 	"github.com/aimeelaplant/comiccruncher/comic"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -23,28 +24,10 @@ func TestCharacterMarshalJSON(t *testing.T) {
 	c := &comic.Character{
 		Name: "test",
 		VendorImage: "test.jpg",
-		Slug: "test",
+		Slug: "test-test",
 	}
 	th := &comic.CharacterThumbnails{
-		Slug: "test",
-	}
-	ch := NewCharacter(c, th)
-
-	bts, err := ch.MarshalJSON()
-
-	expected := `{"publisher":{"name":"","slug":""},"name":"test","other_name":"","description":"","vendor_url":"","vendor_description":"","image":"","vendor_image":"https://d2jsu6fyd1g4ln.cloudfront.net/test.jpg","thumbnails":null}`
-	assert.Nil(t, err)
-	assert.Equal(t, expected, string(bts))
-}
-
-func TestCharacterMarshalJSONThumbs(t *testing.T) {
-	c := &comic.Character{
-		Name: "test",
-		VendorImage: "test.jpg",
-		Slug: "test",
-	}
-	th := &comic.CharacterThumbnails{
-		Slug: "test",
+		Slug: "test-test",
 		VendorImage: &comic.ThumbnailSizes{
 			Small: "test.jpg",
 			Medium: "test.jpg",
@@ -53,8 +36,41 @@ func TestCharacterMarshalJSONThumbs(t *testing.T) {
 	}
 	ch := NewCharacter(c, th)
 	bts, err := ch.MarshalJSON()
-	expected := `{"publisher":{"name":"","slug":""},"name":"test","other_name":"","description":"","vendor_url":"","vendor_description":"","image":"","vendor_image":"https://d2jsu6fyd1g4ln.cloudfront.net/test.jpg","thumbnails":{"slug":"test","image":null,"vendor_image":{"small":"https://d2jsu6fyd1g4ln.cloudfront.net/test.jpg","medium":"https://d2jsu6fyd1g4ln.cloudfront.net/test.jpg","large":"https://d2jsu6fyd1g4ln.cloudfront.net/test.jpg"}}}`
+	assert.Nil(t, err)
+
+	expected := `{"publisher":{"name":"","slug":""},"name":"test","other_name":"","description":"","vendor_url":"","vendor_description":"","slug":"test-test","image":"","vendor_image":"https://d2jsu6fyd1g4ln.cloudfront.net/test.jpg","thumbnails":{"slug":"test-test","image":null,"vendor_image":{"small":"https://d2jsu6fyd1g4ln.cloudfront.net/test.jpg","medium":"https://d2jsu6fyd1g4ln.cloudfront.net/test.jpg","large":"https://d2jsu6fyd1g4ln.cloudfront.net/test.jpg"}}}`
+	assert.Equal(t, expected, string(bts))
+
+	m := make(map[string]interface{})
+	err = json.Unmarshal(bts, &m)
 
 	assert.Nil(t, err)
-	assert.Equal(t, expected, string(bts))
+
+	assertions := map[string]string{
+		"name": "test",
+		"other_name": "",
+		"description": "",
+		"vendor_description": "",
+		"vendor_url": "",
+		"slug": "test-test",
+		"image": "",
+		"vendor_image": "https://d2jsu6fyd1g4ln.cloudfront.net/test.jpg",
+	}
+
+	for key, val := range assertions {
+		assertKeyHasValue(t, key, val, m)
+	}
+	_, ok := m["publisher"]
+	assert.True(t, ok)
+
+	thumbs, ok := m["thumbnails"]
+	assert.True(t, ok)
+	ths := thumbs.(map[string]interface{})
+	assertKeyHasValue(t, "slug", "test-test", ths)
+}
+
+func assertKeyHasValue(t *testing.T, key, value string, m map[string]interface{}) {
+	val, ok := m[key]
+	assert.True(t, ok)
+	assert.Equal(t, value, val)
 }
