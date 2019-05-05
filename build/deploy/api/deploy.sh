85559b455e35d82fc3b96f7173768ceb903139ae
Aimee LaPlant <aimee@aimeelaplant.com>
2019-05-05 01:49:40 -0400

    Fix script

--- build/deploy/api/deploy.sh
+++ build/deploy/api/deploy.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-eval $(aws ecr get-login --no-include-email)
+eval $(~/.local/bin/aws ecr get-login --no-include-email)
 docker-compose pull
 HOSTNAME=${HOSTNAME} docker-compose up -d --build --remove-orphans
 docker system prune -af
+docker logout
